2a72aff90c139a60c41a524c8aa086cbb4ea2697
sykim-stack <sykim.spotiq@gmail.com>
2026-05-17 17:16:41 +0900

    refactor: brainpool-os 내부 복사, 절대경로 제거

---
+++ brain-engine/core/engines/chat/index.js
@@ -0,0 +1,5 @@
+﻿@'
+export { run as room } from './room.js';
+export { run as message } from './message.js';
+export { run as poll } from './poll.js';
+'@ | Set-Content C:\brainpool-os\engines\chat\index.js -Encoding UTF8
