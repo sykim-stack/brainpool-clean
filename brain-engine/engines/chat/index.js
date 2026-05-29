3ff3dead0c8fcba402863cbaa83dbf8f298c8119
sykim-stack <sykim.spotiq@gmail.com>
2026-05-29 17:21:44 +0900

    TASK-02: /api/chat 단일 라우트 전환 (action 분기)

--- brain-engine/engines/chat/index.js
+++
@@ -1,9 +0,0 @@
-// brain-engine/engines/chat/index.js
-
-const message = require('./message');
-const room    = require('./room');
-
-// rooms/route.ts, send/route.ts 호환
-const ChatEngine = { message, room };
-
-module.exports = { message, room, ChatEngine };
